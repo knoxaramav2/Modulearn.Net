3e81514eb6ef6dfbb67d5166d2024477ce023efa
knoxaramav2 <wtscott@iastate.edu>
2024-03-30 14:15:36 -0500

    Database setup

---
+++ Modulearn/Modulearn/Data/Migrations/20240330182053_InitML.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modulearn.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitML : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
